ed1e149a47f256fddba8d8ca8406913869c6fc2d
atdiar <atdiar@gmail.com>
2023-12-05 06:30:04 +0100

    fix cli

--- projectlauncher/zui/cmd/init.go
+++ projectlauncher/zui/cmd/init.go
@@ -614,7 +614,7 @@ func Build(outputPath string, buildTags []string, cmdArgs ...string) error {
 		cmd := exec.Command("go", args...)
 		cmd.Dir = filepath.Join(".")
 		if isWasm{
-			cmd.Dir  = filepath.Join(".")
+			cmd.Dir  = filepath.Join(".", "dev")
 		}
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
