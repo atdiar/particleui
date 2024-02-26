e78b6e570e0dca3f36ec28e8e9f86d40440bb6d5
atdiar <atdiar@gmail.com>
2024-02-26 01:30:14 +0100

    some change to the compiler asset generator

--- drivers/js/repl/cmd/precompiler/gowasm.go
+++ drivers/js/repl/cmd/precompiler/gowasm.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"flag"
 	"fmt"
+
 	//"io"
 	"errors"
 	"os"
@@ -21,6 +23,11 @@ var (
 	d       *bool
 
 	importcfgpath string
+	DefaultDeps   = []string{
+		"github.com/atdiar/particleui",
+		"github.com/atdiar/particleui/drivers/js/compat",
+		"github.com/atdiar/particleui/drivers/js",
+	}
 )
 
 func init() {
@@ -39,23 +46,31 @@ func main() {
 	if *v {
 		verbose = true
 	}
+
 	version, err := getGoVersion()
 	if err != nil {
-		panic(err)
+		fmt.Printf("failed to get Go version: %v", err)
+		os.Exit(1)
 	}
-	
+
+	// everything should be put in the wasmgc directory created from the current working directory
+	err = os.MkdirAll("wasmgc", 0755)
+	if err != nil {
+		fmt.Printf("Unable to create the destination directory: %v \n", err)
+		os.Exit(1)
+	}
+
+	dir := filepath.Join(".", "wasmgc")
 
 	// Incorporate Go version into compiledDir path
-	compiledDir, err := filepath.Abs("./lib_prec_" + version)
-	if err != nil {
-		panic(err)
-	}
+	libDir := filepath.Join(dir, "library")
 
 	// Load or initialize importmap
-	importcfgpath := filepath.Join(compiledDir, "importcfg")
+	importcfgpath = filepath.Join(dir, "importcfg")
 	importmap, err := loadImportMap(importcfgpath)
 	if err != nil {
-		panic(err)
+		fmt.Printf("failed to load importcfg: %v", err)
+		os.Exit(1)
 	}
 
 	if importmap == nil {
@@ -63,43 +78,129 @@ func main() {
 		// we need to compile the standard library
 
 		// Compile the standard library
-		if err := compileStandardLibrary(compiledDir); err != nil {
-			panic(err)
+		if err = compileStandardLibrary(libDir); err != nil {
+			fmt.Printf("failed to compile standard library: %v", err)
+			os.Exit(1)
 		}
 
-		if err = generateImportCfg(compiledDir, &importmap); err != nil {
-			panic(err)
+		// Create the importcfg file
+		if err = generateImportCfg(dir, libDir, &importmap); err != nil {
+			fmt.Printf("failed to create importcfg: %v", err)
+			os.Exit(1)
+		}
+	}
+
+	// TODO make sure that the compilation occurs with build tag !server
+	for _, dep := range DefaultDeps {
+		if err = compileNonStandardPkg(dep, libDir); err != nil {
+			fmt.Printf("failed to compile %s: %v", dep, err)
+			os.Exit(1)
 		}
 	}
 
 	if library != "" {
-		if err := compileNonStdImports(library, compiledDir, importmap); err != nil {
-			panic(err)
+		if err := compileNonStandardPkg(library, libDir); err != nil {
+			fmt.Printf("failed to compile %s: %v", library, err)
+			os.Exit(1)
 		}
 	}
 
-	// Let's compilte the toolchain for wasm
-	err = compileGoToolForWASM("compile", compiledDir)
-	if err != nil {
-		fmt.Errorf("failed to compile go tool compile: %v", err)
+	// Update the importcfg file
+	if err = generateImportCfg(dir, libDir, &importmap); err != nil {
+		fmt.Printf("failed to create importcfg: %v", err)
 		os.Exit(1)
 	}
 
-	err = compileGoToolForWASM("link", compiledDir)
+	// Let's compile the toolchain for wasm
+	err = compileGoToolForWASM("compile", dir)
 	if err != nil {
-		fmt.Errorf("failed to compile go tool link: %v", err)
+		fmt.Printf("failed to compile go tool compile: %v", err)
 		os.Exit(1)
 	}
 
-	err = compileGoToolForWASM("gofmt", compiledDir)
+	err = compileGoToolForWASM("link", dir)
 	if err != nil {
-		fmt.Errorf("failed to compile go tool fmt: %v", err)
+		fmt.Printf("failed to compile go tool link: %v", err)
+		os.Exit(1)
+	}
+
+	err = compileGoToolForWASM("gofmt", dir)
+	if err != nil {
+		fmt.Printf("failed to compile go tool fmt: %v", err)
 		os.Exit(1)
 	}
 
 	if verbose {
-		fmt.Printf("Compilation complete, files are in: %s\n", compiledDir)
+		fmt.Printf("Compilation complete, files are in: %s\n", dir)
 	}
+
+	// Now we should generate the prefetchlist file which is equivalent to the
+	// These will essentially be the list of dependencies that the browser will start fetching by default
+	// and store in indexedDB if not already in there.
+	// This is a way to reduce the time it takes to load the application.
+	var prefetchList = make(map[string]string)
+	for _, dep := range DefaultDeps {
+		d, err := getDependencies(dep)
+		if err != nil {
+			fmt.Printf("Unable to generate prefetch list: failed to get dependencies for %s: %v", dep, err)
+			os.Exit(1)
+		}
+		for _, pkg := range d {
+			pkgpath, ok := importmap[pkg]
+			if !ok {
+				panic("The package " + pkg + " is not in the importmap in spite of being a dependency of the compiled package" + dep)
+			}
+			prefetchList[pkg] = pkgpath
+		}
+	}
+	// Write the prefetch list to a json file
+	prefetchListPath := filepath.Join(dir, "prefetchlist.json")
+	file, err := os.Create(prefetchListPath)
+	if err != nil {
+		fmt.Printf("Unable to create prefetchlist file: %v", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	err = json.NewEncoder(file).Encode(prefetchList)
+	if err != nil {
+		fmt.Printf("Unable to encode prefetchlist: %v", err)
+		os.Exit(1)
+	}
+
+	// let's create manifest.json
+	manifest := map[string]string{
+		"goversion":    version,
+		"importcfg":    filepath.Join(dir, "importcfg"),
+		"prefetchlist": prefetchListPath,
+		"compile":      filepath.Join(dir, "compile.wasm"),
+		"link":         filepath.Join(dir, "link.wasm"),
+		"gofmt":        filepath.Join(dir, "gofmt.wasm"),
+		"libraries":    libDir,
+	}
+
+	manifestPath := filepath.Join(dir, "manifest.json")
+	file, err = os.Create(manifestPath)
+	if err != nil {
+		fmt.Printf("Unable to create manifest file: %v", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	err = json.NewEncoder(file).Encode(manifest)
+	if err != nil {
+		fmt.Printf("Unable to encode manifest: %v", err)
+		os.Exit(1)
+	}
+
+	if verbose {
+		fmt.Printf("Manifest file created at: %s\n", manifestPath)
+	}
+
+	if verbose {
+		fmt.Println("SUCCESS!")
+	}
+
 }
 
 func compileStandardLibrary(targetDir string) error {
@@ -131,50 +232,47 @@ func compileStandardLibrary(targetDir string) error {
 		fmt.Println("Error compiling stdlib:", err)
 		return err
 	}
-
-	fmt.Println("Successfully compiled stdlib to", targetDir)
+	if verbose {
+		fmt.Println("Successfully compiled stdlib to", targetDir)
+	}
 	return nil
 }
 
-// compileNonStdImports compiles a given library and its transtitive dependencies.
-func compileNonStdImports(library, targetDir string, importmap map[string]string) error {
-	// Compile your library and its dependencies
+func compileNonStandardPkg(library, targetDir string) error {
 	if verbose {
-		fmt.Println("Compiling library and its dependencies...")
+		fmt.Printf("Compiling %s and its dependencies...\n", library)
 	}
 
-	// Get dependencies of the library
-	deps, err := getDependencies(library)
+	// Transform the library import path into a directory structure
+	libraryPath := filepath.Join(targetDir, filepath.FromSlash(library))
+	// Ensure that the directory structure exists
+	if err := os.MkdirAll(libraryPath, 0755); err != nil {
+		return fmt.Errorf("failed to create directory structure for %s: %v", library, err)
+	}
+
+	args := []string{"install", "-a"}
+	if verbose {
+		args = append(args, "-v")
+	}
+
+	if debug {
+		//args = append(args, "-n")
+		args = append(args, "-work")
+	}
+	 // filepath.Dir(libraryPath) // ??
+	args = append(args, "-tags", "!server", "-pkgdir", targetDir, library)
+
+	cmd := exec.Command("go", args...)
+	cmd.Env = append(os.Environ(), "GOARCH=wasm", "GOOS=js", "GODEBUG=installgoroot=all")
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to compile %s: %v", library, err)
 	}
 
-	// Open the importcfg file for modification, appending at the end, or create it if it doesn't exist
-	importcfg, err := os.OpenFile(importcfgpath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return fmt.Errorf("failed to open importcfg file: %v", err)
-	}
-	defer importcfg.Close()
-
-	// Compile each package in the dependency list
-	for _, pkg := range deps {
-		if _, exists := importmap[pkg]; !exists { // Check if already compiled
-			if err := compilePackage(pkg, targetDir, importmap); err != nil {
-				fmt.Printf("Failed to compile %s: %v\n", pkg, err)
-			}
-			// append to importcfg
-			importPath := strings.Replace(pkg, filepath.Dir(targetDir)+string(os.PathSeparator), "", 1)
-			importPath = strings.Replace(importPath, string(os.PathSeparator), "/", -1)
-			entry := fmt.Sprintf("packagefile %s=%s\n", importPath, importmap[pkg])
-			if verbose {
-				fmt.Printf("Adding to importcfg: %s", entry)
-			}
-			_, err = importcfg.WriteString(entry)
-			if err != nil {
-				return fmt.Errorf("failed to write to importcfg file: %v", err)
-			}
-
-		}
+	if verbose {
+		fmt.Printf("Successfully compiled %s to %s\n", library, libraryPath)
 	}
 	return nil
 }
@@ -185,72 +283,21 @@ func getDependencies(pkg string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
+	// DEBUG
+	fmt.Println("the list of dependencies for", pkg, "is", string(stdout))
+
 	return strings.Split(strings.TrimSpace(string(stdout)), "\n"), nil
 }
 
-func compilePackage(pkg string, targetDir string, importmap map[string]string) error {
-	// Find source files for the package
-	srcFiles, err := getSourceFiles(pkg)
-	if err != nil {
-		return err
-	}
-
-	if len(srcFiles) == 0 {
-		return nil // No source files, skip package
-	}
-
-	baseDir := filepath.Base(targetDir)
-
-	// Compile the package
-	pkgDir := filepath.Join(targetDir, pkg)
-	err = os.MkdirAll(pkgDir, 0777)
-	if err != nil {
-		return err
-	}
-
-	output := filepath.Join(pkgDir, filepath.Base(pkg)+".a")
-	args := append([]string{"tool", "compile", "-std", "-o", output, "-p", pkg}, srcFiles...)
-	cmd := exec.Command("go", args...)
-	if err := cmd.Run(); err != nil {
-		return err
-	}
-
-	// Update importmap upon successful compilation
-	importmap[pkg] = filepath.Join(baseDir,output)
-
-	if verbose {
-		fmt.Printf("Compiled %s\n", pkg)
-	}
-
-	return nil
-}
-
-func getSourceFiles(pkg string) ([]string, error) {
-	// 'go list -f "{{.GoFiles}}" pkg' to get source files
-	cmd := exec.Command("go", "list", "-f", "{{range .GoFiles}}{{$.Dir}}/{{.}} {{end}}", pkg)
-	stdout, err := cmd.Output()
-	if err != nil {
-		return nil, err
-	}
-	files := strings.Fields(strings.TrimSpace(string(stdout)))
-	return files, nil
-}
-
-func defaultGOROOT() string {
-	cmd := exec.Command("go", "env", "GOROOT")
-	stdout, err := cmd.Output()
-	if err != nil {
-		panic(err)
-	}
-	return strings.TrimSpace(string(stdout))
-}
-
 // Walks through the directory to find all .a files and maps them.
-func generateImportMap(targetDir string) (map[string]string, error) {
+func generateImportMap(baseDir, targetDir string) (map[string]string, error) {
 	importMap := make(map[string]string)
-	baseDir:= filepath.Base(targetDir)
+	targetDirPath, err := filepath.Rel(baseDir, targetDir)
+	if err != nil {
+		return importMap, err
+	}
 
-	err := filepath.Walk(targetDir, func(path string, info os.FileInfo, err error) error {
+	err = filepath.Walk(targetDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
@@ -262,7 +309,7 @@ func generateImportMap(targetDir string) (map[string]string, error) {
 			}
 			importPath := strings.TrimSuffix(relPath, ".a")
 			importPath = strings.ReplaceAll(importPath, string(filepath.Separator), "/")
-			importMap[importPath] = filepath.Join(baseDir, relPath)
+			importMap[importPath] = filepath.Join(filepath.Base(baseDir), targetDirPath, relPath)
 		}
 		return nil
 	})
@@ -271,8 +318,8 @@ func generateImportMap(targetDir string) (map[string]string, error) {
 }
 
 // Generates an importcfg file from the .a files in targetDir.
-func generateImportCfg(targetDir string, pImportmap *map[string]string) error {
-	importMap, err := generateImportMap(targetDir)
+func generateImportCfg(baseDir, targetDir string, pImportmap *map[string]string) error {
+	importMap, err := generateImportMap(baseDir, targetDir)
 	if err != nil {
 		return fmt.Errorf("error generating import map: %v", err)
 	}
@@ -281,11 +328,9 @@ func generateImportCfg(targetDir string, pImportmap *map[string]string) error {
 		imap[k] = v
 	}
 
-	// Write the importcfg file
-	importcfgPath := filepath.Join(targetDir, "importcfg")
-	file, err := os.Create(importcfgPath)
+	file, err := os.Create(importcfgpath)
 	if err != nil {
-		return fmt.Errorf("failed to create importcfg file: %v", err)
+		return fmt.Errorf("failed to open importcfg file: %v", err)
 	}
 	defer file.Close()
 
@@ -297,7 +342,10 @@ func generateImportCfg(targetDir string, pImportmap *map[string]string) error {
 		}
 	}
 
-	fmt.Println("importcfg file generated successfully at:", importcfgPath)
+	if verbose {
+		fmt.Println("importcfg file generated successfully at:", importcfgpath)
+	}
+
 	return nil
 }
 
@@ -353,8 +401,12 @@ func getGoVersion() (string, error) {
 func compileGoToolForWASM(toolName string, targetDir string) error {
 	goroot := runtime.GOROOT()
 	toolPath := filepath.Join(goroot, "src", "cmd", toolName)
+	abstargetDir, err := filepath.Abs(targetDir)
+	if err != nil {
+		return err
+	}
 
-	cmd := exec.Command("go", "build", "-o", filepath.Join(targetDir,fmt.Sprintf("%s.wasm", toolName)), ".")
+	cmd := exec.Command("go", "build", "-o", filepath.Join(abstargetDir, fmt.Sprintf("%s.wasm", toolName)), ".")
 	cmd.Env = append(os.Environ(), "GOOS=js", "GOARCH=wasm")
 	cmd.Dir = toolPath
 
@@ -365,7 +417,10 @@ func compileGoToolForWASM(toolName string, targetDir string) error {
 		return err
 	}
 
-	fmt.Printf("%s compiled successfully to %s.wasm\n", toolName, toolName)
+	if verbose {
+		fmt.Printf("%s compiled successfully to %s.wasm\n", toolName, toolName)
+
+	}
 	return nil
 }
 
@@ -445,15 +500,3 @@ func ExtractImports(filePath string) ([]string, error) {
 
 	return imports, scanner.Err()
 }
-
-/*
-func main() {
-    // Adjust these paths as necessary for your setup
-    err := GenerateImportCfgLink("main.go", "importcfg", "importcfg.link")
-    if err != nil {
-        fmt.Printf("Error generating importcfg.link: %v\n", err)
-    } else {
-        fmt.Println("Successfully generated importcfg.link")
-    }
-}
-*/
